2e1e44ac5cfe5dca85116b302c40a3ff40f910cb
alysonGuimaraes <116230343+alysonGuimaraes@users.noreply.github.com>
2022-10-20 00:52:58 +0000

    Adição de variáveis e exibir elas na tela

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-#Primeiro projeto Python!
-#print() - comando de saída
+# Primeiro projeto Python!
+# print() - comando de saída
 print('Hello, World!')
+
+# Quando quiser guardar uma String
+nome = 'Alyson Guimarães'
+
+# Quando quiser guardar número inteiro
+numero = 99
+
+# Exibir o nome que está dentro da variável nome
+print(nome)
+
+# Exibir o número que está dentro da variável numero
+print(numero)
